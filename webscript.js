60acc4d15bcc7b233dce8345941d1db981dd6211
emasudaualberta <emasuda@ualberta.ca>
2024-02-27 23:04:04 -0700

    i think i made navigation bar

--- webscript.js
+++ webscript.js
@@ -1,3 +1,34 @@
+var hovering = 0;
+
 function onFocus(text){
-    document.getElementById("mainbar").placeholder = text
+    document.getElementById("mainbar").placeholder = text;
   }
+function showDropdown() {
+    document.getElementById("myDropdown").style.display = "block";
+}
+
+function hideDropdown(){
+    if (hovering ==0){
+        document.getElementById("myDropdown").style.display = "none";
+    }
+}
+function isHovering(bool){
+    hovering = bool;
+}
+
+function filterFunc(){
+    var i;
+    var input = document.getElementById("mainbar");
+    var typed = input.value.toUpperCase();
+    var div = document.getElementById("myDropdown");
+    var element = div.getElementsByClassName("dropdown");
+    for(i =0; i<element.length;i++){
+        txtValue = element[i].textContent || element[i].innerText;
+        if (txtValue.toUpperCase().indexOf(typed) > -1) {
+            element[i].style.display = "";
+        } else {
+            element[i].style.display = "none";
+          }
+    }
+
+}
