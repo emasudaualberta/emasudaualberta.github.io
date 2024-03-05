dfe5935538f37bacb5b253f6c842ba49fadcb325
ethan <emasuda@ualberta.ca>
2024-03-05 03:29:50 -0700

    added project div and projectr div animation and automation

--- webscript.js
+++ webscript.js
@@ -1,4 +1,4 @@
-var hovering = 0;
+
 
 function onFocus(text){
     document.getElementById("mainbar").placeholder = text;
@@ -10,11 +10,7 @@ function showDropdown() {
     bar.style.borderBottomRightRadius = '0px';
 }
 
-function hideDropdown(){
-    if (hovering ==0){
-        document.getElementById("myDropdown").style.display = "none";
-    }
-}
+
 function isHovering(bool){
     hovering = bool;
 }
@@ -37,7 +33,7 @@ function filterFunc(){
 }
 window.onclick = function(e) {
     if (!e.target.matches('.par, .homebar, .dropdownDiv,.dropdown')) {
-    var myDropdown = document.getElementById("myDropdown");
+      var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.style.display == 'block') {
         myDropdown.style.display = 'none';
         bar = document.getElementById("mainbar");
@@ -45,4 +41,47 @@ window.onclick = function(e) {
         bar.style.borderBottomRightRadius = '20px';
       }
     }
+}
+
+const projectList = ['caffcalc','squish','webportfolio','me','meep','pennn']
+
+window.onload = () => {
+  
+  for(project in projectList){
+    const layers = ['imageStackTop','imageStackMid','imageStackBottom'];
+    var someLink = projectList[project]+'/'+projectList[project];
+    var projectHolder = document.getElementById('projectHolder');
+    var container = document.createElement("div");
+    container.classList.add("container");
+    var a = document.createElement('a');
+    a.href = someLink+'.html';
+    var display = document.createElement("div");
+    display.classList.add("projDisplay");
+
+    for(var i =1; i<4; i++){
+      var elementCont = document.createElement('div');
+      elementCont.classList.add(layers[i-1]);
+      var image = document.createElement('img');
+      var int  = i.toString();
+      image.src = someLink +int +'.png';
+      elementCont.appendChild(image);
+      display.appendChild(elementCont);
+    }
+
+
+    var node = document.createTextNode(projectList[project]);
+    var title = document.createElement('p');
+    title.appendChild(node);
+
+
+    a.appendChild(display);
+    a.appendChild(title);
+    container.appendChild(a);
+    try{
+    projectHolder.appendChild(container);
+    }catch (e){
+      window.alert(e)
+    }
   }
+
+}
